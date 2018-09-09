3e80d9d0433374b495facf9aa46c91ec73a0d366
TuanNguyenNaem <41229979+TuanNguyenNaem@users.noreply.github.com>
2018-09-09 09:54:28 +0700

    Complete Edit Book

--- BooksStore_Management/Areas/Administrator/Controllers/SanPhamController.cs
+++ BooksStore_Management/Areas/Administrator/Controllers/SanPhamController.cs
@@ -176,8 +176,133 @@ namespace BooksStore_Management.Areas.Administrator.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Edit()
+        public ActionResult Edit(
+            int idSach,
+            string tenSach,
+            string tacGia,
+
+            bool add_TacGia,
+            string tenTacGia,
+            string gioiTinhTG,
+            DateTime? ngaySinhTG,
+            string diaChiTG,
+
+            string nhaXuatBan,
+
+            bool add_NhaXuatBan,
+            string tenNhaXuatBan,
+            string diaChiNXB,
+            string soDienThoaiNXB,
+
+            int doTuoi,
+            string danhMuc,
+
+            bool add_DanhMuc,
+            string tenDanhMuc,
+            DateTime? ngayTaoDM,
+            string tinhTrangDM,
+
+            string loaiBia,
+            string khoGiay,
+            DateTime ngayXuatBan,
+            string loaiGiay,
+            int soTrang,
+            string chiTiet,
+            int gia)
         {
+            if (Request.Files.Count > 0)
+            {
+                var image = new string[Request.Files.Count];
+
+                for (int i = 0; i < Request.Files.Count; i++)
+                {
+
+                    var file = Request.Files[i];
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var filename = Path.GetFileName(file.FileName);
+                        var path = Path.Combine(Server.MapPath("~/Content/Images/"), filename);
+                        file.SaveAs(path);
+                        image[i] = filename;
+                    }
+                }
+                var sach = context.Sach.FirstOrDefault(c=>c.MaS == idSach);
+                var ctSach = context.CTSach.FirstOrDefault(c=>c.MaS == idSach);
+                var anhSach = context.AnhSach.FirstOrDefault(c=>c.MaS == idSach);
+
+                //update Sach
+                sach.MaDT = doTuoi;
+                if (add_TacGia)
+                {
+                    var tac_Gia = new TacGia()
+                    {
+                        TenTG = tenTacGia,
+                        GioiTinh = gioiTinhTG,
+                        NgaySinh = ngaySinhTG,
+                        DiaChi = diaChiTG
+                    };
+                    context.TacGia.Add(tac_Gia);
+                    context.SaveChanges();
+                    sach.MaTG = tac_Gia.MaTG;
+                }
+                else
+                {
+                    sach.MaTG = int.Parse(tacGia);
+                }
+                if (add_NhaXuatBan)
+                {
+                    var nha_Xuat_Ban = new NhaXuatBan()
+                    {
+                        TenNXB = tenNhaXuatBan,
+                        DiaChi = diaChiNXB,
+                        Sdt = soDienThoaiNXB
+                    };
+                    context.NhaXuatBan.Add(nha_Xuat_Ban);
+                    context.SaveChanges();
+                    sach.MaNXB = nha_Xuat_Ban.MaNXB;
+                }
+                else
+                {
+                    sach.MaNXB = int.Parse(nhaXuatBan);
+                }
+                if (add_DanhMuc)
+                {
+                    var danh_Muc = new DanhMuc()
+                    {
+                        TenDM = tenDanhMuc,
+                        NgayTao = DateTime.Today,
+                        TinhTrang = "Hoạt động"
+                    };
+                    context.DanhMuc.Add(danh_Muc);
+                    context.SaveChanges();
+                    sach.MaDM = danh_Muc.MaDM;
+                }
+                else
+                {
+                    sach.MaDM = int.Parse(danhMuc);
+                }
+                context.SaveChanges();
+
+                //Update CTSach
+                ctSach.TenS = tenSach;
+                ctSach.LoaiBia = loaiBia;
+                ctSach.NgayXuatBan = ngayXuatBan;
+                ctSach.Kho = khoGiay;
+                ctSach.SoTrang = soTrang;
+                ctSach.Gia = gia;
+                ctSach.LoaiGiay = loaiGiay;
+                ctSach.ChiTiet = chiTiet;
+                context.SaveChanges();
+
+                //Update AnhSach
+                anhSach.Anh1 = "/Content/Images/" + image[0];
+                anhSach.Anh2 = "/Content/Images/" + image[1];
+                anhSach.Anh3 = "/Content/Images/" + image[2];
+                anhSach.Anh4 = "/Content/Images/" + image[3];
+                anhSach.Anh5 = "/Content/Images/" + image[4];
+                anhSach.Anh6 = "/Content/Images/" + image[5];
+                context.SaveChanges();
+            }
             return Redirect("/Administrator/SanPham");
         }
     }
