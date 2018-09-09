34687bf7ef2a039eef1d98bfee6cd6329a46face
TuanNguyenNaem <41229979+TuanNguyenNaem@users.noreply.github.com>
2018-09-09 16:52:21 +0700

    Complete KhuyenMai

--- BooksStore_Management/Areas/Administrator/Controllers/KhuyenMaiController.cs
+++ BooksStore_Management/Areas/Administrator/Controllers/KhuyenMaiController.cs
@@ -19,8 +19,46 @@ namespace BooksStore_Management.Areas.Administrator.Controllers
         // GET: Administrator/KhuyenMai
         public ActionResult Index()
         {
+            ViewBag.KhuyenMai = context.KhuyenMai.ToList();
             var listSach = context.Sach.ToList();
             return View(listSach);
+        }
+        public ActionResult DangKhuyenMai()
+        {
+            ViewBag.KhuyenMai = context.KhuyenMai.Where(c=>c.BatDau <= DateTime.Now && DateTime.Now <= c.KetThuc).ToList();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ThemMoi(int id)
+        {
+            ViewBag.Item = id;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ThemMoi(
+            int maSach,
+            double giamGia,
+            DateTime ngayBatDau,
+            DateTime ngayKetThuc,
+            string tenSuKien)
+        {
+            var kM = context.KhuyenMai.FirstOrDefault(c => c.MaS == maSach);
+            if (kM != null)
+            {
+                context.KhuyenMai.Remove(kM);
+                context.SaveChanges();
+            }
+            var khuyenMai = new KhuyenMai()
+            {
+                MaS = maSach,
+                KhuyenMai1 = giamGia,
+                BatDau = ngayBatDau,
+                KetThuc = ngayKetThuc,
+                TenSuKien = tenSuKien
+            };
+            context.KhuyenMai.Add(khuyenMai);
+            context.SaveChanges();
+            return Redirect("/Administrator/KhuyenMai/Index");
         }
     }
 }
